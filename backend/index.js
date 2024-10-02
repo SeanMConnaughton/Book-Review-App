bbaa8fc0dc3c40f3b5c5ee3873c535977a787b91
Sean Connaughton <smc56462@gmail.com>
2024-10-02 16:33:43 -0400

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(cors());
 app.use("/books", bookRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Book API');
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
